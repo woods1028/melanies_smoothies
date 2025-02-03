b08381453cd6ba2c5939fad55d1fcebc6b054096
woods1028 <woods1028@gmail.com>
2025-02-03 11:55:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 #from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
